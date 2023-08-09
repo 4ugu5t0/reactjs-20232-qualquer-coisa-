3a98cde585beb357ea98a87a00e0044dcd8c0d92
Augusto <augusto.BTE@proton.me>
2023-08-08 22:42:07 -0300

    segunda aula

---
+++ exemplo/src/components/Produto.tsx
@@ -0,0 +1,18 @@
+
+// vamos definir as props (propriedades) do componente Produto
+interface ProdutoProps {
+    nome: string,
+    descricao: string,
+    qtde: number,
+    preco: number
+}
+export function Produto(prod: ProdutoProps) {
+    return (
+        <div className="bg-zinc-400 w-30 h-10 text-white rounded m-2 flex items-center justify-center">
+            Nome: {prod.nome}
+            Descrição: {prod.descricao}
+            Qtde: {prod.qtde}
+            Preço: {prod.preco}
+        </div>
+    )
+}
